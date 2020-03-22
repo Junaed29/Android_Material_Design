be5835af050876decc138e5bc817ceb01d2fb89b
SadiaTasnim259 <tasnimsadia259@gmail.com>
2020-03-22 23:04:04 +0600

    Validation of registration added

--- app/src/main/java/com/example/androidmaterialdesign/LoginActivity.java
+++ app/src/main/java/com/example/androidmaterialdesign/LoginActivity.java
@@ -2,13 +2,73 @@ package com.example.androidmaterialdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.imageView2)
+    ImageView imageView;
+
+    @BindView(R.id.welcomeTextView)
+    TextView welcomeTextView;
+
+    @BindView(R.id.continueTextView)
+    TextView continueTextView;
+
+    @BindView(R.id.userNameEditTextId)
+    TextInputLayout userNameEditText;
+
+    @BindView(R.id.passwordEditTextId)
+    TextInputLayout passwordEditText;
+
+    @BindView(R.id.logInButtonId)
+    Button logInButton;
+
+    @BindView(R.id.signUpButtonId)
+    Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    public void regButton(View view) {
+        Intent intent = new Intent(LoginActivity.this,RegistrationActivity.class);
+        Pair [] pairs = new Pair[7];
+        pairs[0] = new Pair<View,String>(imageView, "logo_image");
+        pairs[1] = new Pair<View,String>(welcomeTextView, "logo_title");
+        pairs[2] = new Pair<View,String>(continueTextView, "welcome_title");
+        pairs[3] = new Pair<View,String>(userNameEditText, "userName_Etx");
+        pairs[4] = new Pair<View,String>(passwordEditText, "password_Etx");
+        pairs[5] = new Pair<View,String>(logInButton, "go_btn");
+        pairs[6] = new Pair<View,String>(signUpButton, "other_btn");
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this,pairs);
+            startActivity(intent,options.toBundle());
+        }else{
+            startActivity(intent);
+        }
+
+        startActivity(intent);
+    }
+
+    public void regButtonClick(View view) {
+        Intent intent = new Intent(LoginActivity.this,RegistrationActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/androidmaterialdesign/RegistrationActivity.java
@@ -0,0 +1,183 @@
+package com.example.androidmaterialdesign;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RegistrationActivity extends AppCompatActivity implements TextWatcher {
+    private static final String TAG = "RegistrationActivity";
+
+    @BindView(R.id.fullNameEditTextIdReg)
+    TextInputLayout fullNameEditText;
+
+    @BindView(R.id.userNameEditTextIdReg)
+    TextInputLayout userNameEditText;
+
+    @BindView(R.id.emailEditTextIdReg)
+    TextInputLayout emailEditText;
+
+    @BindView(R.id.phoneNoEditTextIdReg)
+    TextInputLayout phoneNoEditText;
+
+    @BindView(R.id.passwordEditTextIdReg)
+    TextInputLayout passwordEditText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        ButterKnife.bind(this);
+
+        fullNameEditText.getEditText().addTextChangedListener(this);
+
+
+    }
+
+    // app:errorEnabled="true" create a gap which is helping app:counterEnabled="true"
+    //  For this I comment fullNameEditText.setErrorEnabled(false);
+    public void logInButtonClick(View view) {
+        finish();
+    }
+
+    private Boolean validFullName(String val) {
+        //String val = fullNameEditText.getEditText().getText().toString();
+
+        if (val.isEmpty()) {
+            fullNameEditText.setError("Field can not be empty");
+            return false;
+        } else {
+            fullNameEditText.setError(null);
+            //fullNameEditText.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private Boolean validUserName(String val) {
+        //String val = userNameEditText.getEditText().getText().toString();
+        String noWhiteSpace = "\\A\\w{4,20}\\z";
+
+        Log.d(TAG, "validUserName: "+val.length());
+
+
+        if (val.isEmpty()) {
+            userNameEditText.setError("Field can not be empty");
+            return false;
+        } else if (val.length()<5){
+            Log.d(TAG, "validUserName: "+val.length());
+            userNameEditText.setError("User Name is too short (At least 5 character)");
+            return false;
+        }else if (!val.matches(noWhiteSpace)){
+            userNameEditText.setError("White Spaces are not allowed");
+            return false;
+        } else {
+            userNameEditText.setError(null);
+            //userNameEditText.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private Boolean validEmail(String val) {
+        //String val = emailEditText.getEditText().getText().toString();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (val.isEmpty()) {
+            emailEditText.setError("Field can not be empty");
+            return false;
+        }else if (!val.matches(emailPattern)) {
+            emailEditText.setError("Invalid email address");
+            return false;
+        } else {
+            emailEditText.setError(null);
+            //emailEditText.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private Boolean validPhone(String val) {
+        //String val = phoneNoEditText.getEditText().getText().toString();
+
+        if (val.isEmpty()) {
+            phoneNoEditText.setError("Field can not be empty");
+            return false;
+        } else {
+            phoneNoEditText.setError(null);
+            //phoneNoEditText.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private Boolean validPassword(String val) {
+        //String val = passwordEditText.getEditText().getText().toString();
+        String passwordPattern = "^" +
+                "(?=.*[0-9])" +         //at least 1 digit
+                //"(?=.*[a-z])" +         //at least 1 lower case letter
+                //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+                //"(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=\\S+$)" +           //no white spaces
+                ".{4,}" +               //at least 4 characters
+                "$";
+
+        String checkDigit = "^" + "(?=.*[0-9])" + "(?=.*[a-zA-Z])" + ".{4,}" + "$";
+        String checkWhiteSpace = "^" + "(?=\\S+$)" + "(?=.*[a-zA-Z])" + ".{4,}" + "$";
+
+        if (val.isEmpty()) {
+            passwordEditText.setError("Field can not be empty");
+            return false;
+        }else if (val.length()<8){
+            passwordEditText.setError("Password is too short (At least 8 character)");
+            return false;
+        }else if (!val.matches(checkDigit)) {
+            passwordEditText.setError("Must have at least 1 digit");
+            return false;
+        }
+        else if (!val.matches(checkWhiteSpace)) {
+            passwordEditText.setError("White Spaces are not allowed");
+            return false;
+        } else {
+            passwordEditText.setError(null);
+            //passwordEditText.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    public void regButtonClick(View view) {
+        String password = passwordEditText.getEditText().getText().toString();
+        String fullName = fullNameEditText.getEditText().getText().toString();
+        String userName = userNameEditText.getEditText().getText().toString();
+        String email = emailEditText.getEditText().getText().toString();
+        String phoneNumber = phoneNoEditText.getEditText().getText().toString();
+
+        if (!validFullName(fullName) | !validUserName(userName) | !validEmail(email) | !validPhone(phoneNumber) | !validPassword(password)) {
+            return;
+        }
+
+        Log.d(TAG, "All Clear");
+
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+}
